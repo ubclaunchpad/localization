518d3fa3b4ed9c89ff126c4b07bbec9ace2a92fa
Kevin Zhang <kevinzhang@Kevins-MacBook-Pro.local>
2024-10-05 13:12:59 -0400

    setup i18nilize app and create sample endpoints

--- core/i18nilize/views.py
+++ core/i18nilize/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+class SampleAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'message': 'Hey there!'
+        }
+        return Response(data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        received_data = request.data
+        response_data = {
+            'message': 'Data received successfully!',
+            'received_data': received_data
+        }
+        return Response(response_data, status=status.HTTP_201_CREATED)
