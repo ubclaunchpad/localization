6b2296ae4a0295480cb9f8a9cb61c1cb6fdacbaa
angelafeliciaa <angelafc@students.cs.ubc.ca>
2024-10-12 14:13:38 -0700

    update path, add migration file

--- core/i18nilize/urls.py
+++ core/i18nilize/urls.py
@@ -4,6 +4,6 @@ from .views import CreateTokenView, ReadTokenView
 
 urlpatterns = [
     path("", views.SampleAPIView.as_view(), name="index"),
-    path('api/token/create/', CreateTokenView.as_view(), name='create-token'),
-    path('api/token/read/<int:pk>/', ReadTokenView.as_view(), name='read-token'),
+    path('token/create/', CreateTokenView.as_view(), name='create-token'),
+    path('token/read/<int:pk>/', ReadTokenView.as_view(), name='read-token'),
 ]
