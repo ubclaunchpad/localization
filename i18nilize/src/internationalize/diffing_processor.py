d12f1258f82ea511092511918cecd83125b1f014
Andrew Fenton <andrewfenton898@gmail.com>
2024-11-05 09:56:37 -0500

    added stub functions

---
+++ i18nilize/src/internationalize/diffing_processor.py
@@ -0,0 +1,31 @@
+"""
+Gets differences between old and new states of changed translation
+files.
+"""
+def get_diff():
+    pass
+
+"""
+Updates old file hashes and updates old state of changed files.
+"""
+def update_metadata():
+    pass
+
+"""
+Returns an array of added, modified, and deleted translations
+between two sets of translations.
+"""
+def compare_files(oldTranslations, newTranslations):
+    pass
+
+"""
+Compares hashes of old and new translation files.
+"""
+def get_changed_files():
+    pass
+
+"""
+Returns hash of given file
+"""
+def hash_file(file_path):
+    pass
