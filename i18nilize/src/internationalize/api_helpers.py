8c5f7786fe35bb51c7c8dd1040a33837d709c6f0
angelafeliciaa <angela_felicia@yahoo.com>
2024-11-14 19:47:34 -0800

    new function for generating default language file

--- i18nilize/src/internationalize/api_helpers.py
+++ i18nilize/src/internationalize/api_helpers.py
@@ -1,8 +1,11 @@
 # token_manager.py
+from internationalize.helpers import generate_file
+from core.i18nilize.services.locale import get_default_language
 import globals
 from core.i18nilize.views import TokenView
 from rest_framework.request import Request
 from django.http import HttpRequest
+
 
 def create_token():
     http_request = HttpRequest()
@@ -18,3 +21,20 @@ def create_token():
         print("Token set.")
     else:
         raise Exception(f"Failed to retrieve token. Status code: {response.status_code}")
+    
+def generate_default_language_file():
+    token = globals.token
+    if not token:
+        print("Token not found. Creating a new token...")
+        create_token()
+        token = globals.token
+        if not token:
+            raise Exception("Failed to create token.")
+        
+    language = get_default_language()
+    if not language:
+        raise Exception("Failed to determine the default language.")
+    
+    generate_file(language, token)
+    print(f"Generated translation file for language: {language}")
+
