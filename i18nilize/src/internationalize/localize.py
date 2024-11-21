ae12c54e37972fea6277067a8b776fe8abc86aa6
Stephanie Feng <stephanie.c.feng@gmail.com>
2024-11-21 00:55:24 -0800

    add tests for localize.load_language()

--- i18nilize/src/internationalize/localize.py
+++ i18nilize/src/internationalize/localize.py
@@ -1,46 +1,43 @@
 import json
 import os
-from src.internationalize.helpers import get_json
 
-
-# NOTE: this is the old get_translation function - replace with the new one!!
-def get_translation(file_path, language, word):
-    # Input:
-    #   - file_path: path of json file
-    #   - language: chosen language to translate to
-    #   - word: chosen word to translate
-    # Output: translated word based on chosen language
-    data = get_json(file_path)
-    for translation in data["translations"]:
-        if translation["language"] == language:
-            new_word = translation.get(word)
-            if (new_word is None):
-                return f"'{word}' not found in {language}"
-            return new_word
-    return f"'{language}' not found"
+# from src.internationalize.helpers import get_json
+# # NOTE: this is the old get_translation function - replace with the new one!!
+# def get_translation(file_path, language, word):
+#     # Input:
+#     #   - file_path: path of json file
+#     #   - language: chosen language to translate to
+#     #   - word: chosen word to translate
+#     # Output: translated word based on chosen language
+#     data = get_json(file_path)
+#     for translation in data["translations"]:
+#         if translation["language"] == language:
+#             new_word = translation.get(word)
+#             if (new_word is None):
+#                 return f"'{word}' not found in {language}"
+#             return new_word
+#     return f"'{language}' not found"
 
 
 class Localize:
-    self.languages_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "languages")
-
-    def __init__(self):
-        self.translations_map = {}
+    languages_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "languages")
+    translations_map = {}
 
     @classmethod
-    def load_language(self, language):
+    def load_language(cls, language):
         """
         Load the translation file for a specific language if not already loaded.
         """
-        if language not in self.translations_map:
-            file_path = os.path.join(self.languages_dir, f"{language}.json")
+        if language not in cls.translations_map:
+            file_path = os.path.join(cls.languages_dir, f"{language}.json")
             if os.path.exists(file_path):
                 with open(file_path, "r") as file:
-                    self.translations_map[language] = json.load(file)
+                    cls.translations_map[language] = json.load(file)
             else:
                 raise FileNotFoundError(f"Translations for {language} not found.")
 
     @classmethod
-    def translate(self):
+    def translate(cls):
         """
         Get translation for a word in the specified language.
         """
--- i18nilize/tests/test_localize.py
+++ i18nilize/tests/test_localize.py
@@ -1,15 +1,36 @@
 import unittest
+from unittest.mock import patch
 from src.internationalize.localize import Localize
 
+# to test:
+# in i18nilize directory, run python -m tests.test_localize
 
 class TestLocalize(unittest.TestCase):
     def setUp(self):
-        # do some setting up for the tests
-        pass
+        Localize.translations_map = {}
+    
+    @patch("os.path.exists")
+    def test_load_language_already_in_map(self, mock_path_exists):
+        Localize.translations_map["french"] = { "hello": "bonjour" }
+        Localize.load_language("french")
 
-    def basicTest(self):
-        print(Localize.translate("en", "hello"))  # Outputs the translation if found, else "Translation not found"
-        print(Localize.translate("es", "hello"))  # Similarly checks Spanish language file
+        mock_path_exists.assert_not_called()
+
+    def test_load_existing_language(self):
+        Localize.load_language("spanish")
+
+        self.assertIn("spanish", Localize.translations_map)
+        self.assertEqual(Localize.translations_map["spanish"], { "hello": "hola", "thanks": "gracias" })
+
+    def test_load_non_existing_language(self):
+        with self.assertRaises(FileNotFoundError) as context:
+            Localize.load_language("german")
+        
+        self.assertEqual(str(context.exception), "Translations for german not found.")
+
+    # def basicTest(self):
+    #     print(Localize.translate("en", "hello"))  # Outputs the translation if found, else "Translation not found"
+    #     print(Localize.translate("es", "hello"))  # Similarly checks Spanish language file
 
 
 if __name__ == '__main__':
