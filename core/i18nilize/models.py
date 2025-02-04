867dcbbdb771b1e337cdf4d966dabe17d463406b
Alex Luo <60013046+AlexLuo602@users.noreply.github.com>
2025-02-04 01:59:03 -0800

    Added models for Writer and Microservice Tokens

--- core/i18nilize/models.py
+++ core/i18nilize/models.py
@@ -15,3 +15,18 @@ class Translation(models.Model):
     original_word = models.CharField(max_length = 255)
     translated_word = models.CharField(max_length = 255)
     language = models.CharField(max_length = 255)
+
+class MicroserviceToken(models.Model):
+    microservice_token = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    project_token = models.ForeignKey(Token, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Microservice Token: {str(self.microservice_token)} - Project Token: {str(self.project_token)}"
+
+class Writer(models.Model):
+    project_token = models.ForeignKey(Token, on_delete=models.CASCADE)
+    editor_token = models.ForeignKey(MicroserviceToken, null=True, on_delete=models.SET_NULL, related_name="write_permissions")
+
+    def __str__(self):
+        return f"Project: {self.project_token} - Editor: {self.editor_token or 'None'}"
