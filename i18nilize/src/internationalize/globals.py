86f2f18891afd1487bddef86f0a9f6227b189582
Andrew Fenton <andrewfenton898@gmail.com>
2025-02-25 23:59:30 -0500

    chore: removed token and added comment

--- i18nilize/src/internationalize/globals.py
+++ i18nilize/src/internationalize/globals.py
@@ -4,11 +4,11 @@ import os
 
 class GlobalToken:
     def __init__(self):
-        self.value = "ae54501d-48c1-4a05-80d3-2b4d417de771"
+        self.value = "dummy"
 
 
 CONFIG_FILE_PATH = os.path.expanduser("~/.i18nilize_config.json")
-# Directory values are set dynamically in command_line.py
+# Directory values are set dynamically on startup in command_line.py
 ROOT_DIRECTORY = ""
 LANGUAGES_DIR = ""
 
