3decd47731fbe7bf56fc7e01e767d5a4654862a9
Brian Park <bmp1208@students.cs.ubc.ca>
2025-03-07 17:39:34 -0800

    refactor: remove unnecessary code

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ from setuptools.command.install import install
 import os
 import json
 
-CONFIG_FILE = os.path.expanduser("~/.i18nilize_config.json")
-
 def find_project_root(start_path=None):
     if start_path is None:
         start_path = os.getcwd()
@@ -15,21 +13,6 @@ def find_project_root(start_path=None):
             return current_path  
         current_path = os.path.dirname(current_path) 
     return None 
-
-class OnInstallCommand(install):
-    """Custom install command to save installation directory."""
-    def run(self):
-        install.run(self) 
-
-        install_dir = find_project_root()
-        if install_dir is None:
-            install_dir = os.getcwd() 
-
-        # Save installation directory
-        with open(CONFIG_FILE, "w") as f:
-            json.dump({"installation_dir": install_dir}, f)
-
-        print(f"[INFO] Saved installation directory: {install_dir}")
 
 # setup file for the command line
 # before testing, do the following command:
@@ -49,7 +32,4 @@ setuptools.setup(
     entry_points = {
         'console_scripts': ['i18nilize=i18nilize.src.internationalize.command_line:cli'],
     },
-    cmdclass={
-        'install':OnInstallCommand,
-    }
 )
