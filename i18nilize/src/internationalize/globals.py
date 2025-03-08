184ed7e88452c9315597554c7ee360498bff8c1e
Andrew Fenton <andrewfenton898@gmail.com>
2025-03-07 22:17:22 -0500

    chore: remove unused global constant

--- i18nilize/src/internationalize/globals.py
+++ i18nilize/src/internationalize/globals.py
@@ -1,13 +1,8 @@
 # Test Token: "c84234c3-b507-4ed0-a6eb-8b10116cdef1"
-import os
-
-
 class GlobalToken:
     def __init__(self):
         self.value = "replace_with_valid_token"
 
-
-CONFIG_FILE_PATH = os.path.expanduser("~/.i18nilize_config.json")
 
 # Directory values are set dynamically on startup in command_line.py
 ROOT_DIRECTORY = ""
